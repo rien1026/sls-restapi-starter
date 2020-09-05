81d1a0583cdd20e58c7d1a6126010818494b4661
Sangyeon Cho <rien1026@gmail.com>
2020-09-05 20:57:33 +0900

    Update AppError.ts

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,15 +1,17 @@
 import { logger } from './Logger';
+import { Constants } from './Constants';
 
 export class AppError extends Error {
 	errCode?: number;
 	responseCode?: number;
 	isProgrammerErr?: boolean;
+	logging?: boolean;
 
 	constructor(
 		name: string,
 		message: string,
 		stack: string,
-		errDetail: { errCode?: number; responseCode?: number; isProgrammerErr?: boolean } = {},
+		errDetail: { errCode?: number; responseCode?: number; isProgrammerErr?: boolean; logging?: boolean } = {},
 	) {
 		super();
 		this.name = name;
@@ -18,11 +20,15 @@ export class AppError extends Error {
 		this.responseCode = errDetail.responseCode;
 		this.errCode = errDetail.errCode;
 		this.isProgrammerErr = errDetail.isProgrammerErr;
+		this.logging = typeof errDetail.logging === 'boolean' ? errDetail.logging : true;
 
 		this.doAfterErr();
 	}
 
 	doAfterErr = () => {
+		if (Constants.PROD_MODE === 'prod' && !this.logging) {
+			return;
+		}
 		logger.error({
 			e: this.name + ': ' + this.message,
 			message: ' ',
